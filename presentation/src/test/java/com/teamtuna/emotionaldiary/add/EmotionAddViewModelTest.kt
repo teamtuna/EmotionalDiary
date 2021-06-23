400b5fb5a4c2dadfb0aa190c7f6f8397d6c46fa0
Pennya <rlawlgns077@gmail.com>
2021-06-23 21:19:27 +0900

    EmotionAddViewModelTest 테스트 케이스 추가

--- presentation/src/test/java/com/teamtuna/emotionaldiary/add/EmotionAddViewModelTest.kt
+++ presentation/src/test/java/com/teamtuna/emotionaldiary/add/EmotionAddViewModelTest.kt
@@ -1,20 +1,44 @@
 package com.teamtuna.emotionaldiary.add
 
-import org.junit.After
-import org.junit.Before
+import com.nhaarman.mockitokotlin2.times
+import com.teamtuna.emotionaldiary.entity.Emotion
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.jupiter.MockitoExtension
 
-import org.junit.Assert.*
+@ExperimentalCoroutinesApi
+@DisplayName("감정추가 뷰모델 테스트")
+@ExtendWith(MockitoExtension::class)
+internal class EmotionAddViewModelTest {
 
-class EmotionAddViewModelTest {
+    @Mock
+    private lateinit var viewModel: EmotionAddViewModel
 
-    @Before
-    fun setUp() {
+    @BeforeEach
+    fun setUp() = runBlockingTest {
     }
 
-    @After
+    @AfterEach
     fun tearDown() {
     }
 
+    // org.junit.jupiter.api.Test 사용 필수 (Mock 초기화 안되는 이슈)
+    @Test
+    @DisplayName("기쁨이를 추가 한경우 EmotionAddViewModel.add가 호출되는지확인")
+    fun add() = runBlockingTest {
+        //given
 
+        //when
+        viewModel.add(Emotion.JOY, "기쁨이")
 
+        //then
+        Mockito.verify(viewModel, times(1)).add(Emotion.JOY, "기쁨이")
+    }
 }
