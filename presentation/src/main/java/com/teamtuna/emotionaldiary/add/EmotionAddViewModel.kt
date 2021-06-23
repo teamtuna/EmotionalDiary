f8722c0991a2bde36bd633af2773e0f8404242a0
Pennya <rlawlgns077@gmail.com>
2021-06-23 20:06:54 +0900

    뷰모델에 수정된 내용 반영

--- presentation/src/main/java/com/teamtuna/emotionaldiary/add/EmotionAddViewModel.kt
+++ presentation/src/main/java/com/teamtuna/emotionaldiary/add/EmotionAddViewModel.kt
@@ -3,9 +3,11 @@ package com.teamtuna.emotionaldiary.add
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.teamtuna.emotionaldiary.entity.Emotion
 import com.teamtuna.emotionaldiary.usecase.EmotionAddUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,12 +15,16 @@ class EmotionAddViewModel @Inject constructor(
         private val emotionAddUseCase: EmotionAddUseCase
 ): ViewModel() {
 
-    private val _response = MutableLiveData<Int>()
-    val response : LiveData<Int>
+    private val _response = MutableLiveData<Long>()
+    val response : LiveData<Long>
         get() = _response
 
     fun add(emotion: Emotion, reason: String) {
-        _response.value = emotionAddUseCase(emotion, reason)
+        viewModelScope.launch {
+            emotionAddUseCase(emotion, reason).let {
+                _response.value = it
+            }
+        }
     }
 
 }
