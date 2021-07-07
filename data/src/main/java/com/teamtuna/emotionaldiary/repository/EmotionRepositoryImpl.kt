13044111a1790157c27c61f3459ab53cdbe12e44
babosamo <babosamo@gmail.com>
2021-07-07 23:29:46 +0900

    Result 추가

--- data/src/main/java/com/teamtuna/emotionaldiary/repository/EmotionRepositoryImpl.kt
+++ data/src/main/java/com/teamtuna/emotionaldiary/repository/EmotionRepositoryImpl.kt
@@ -3,14 +3,22 @@ package com.teamtuna.emotionaldiary.repository
 import com.teamtuna.emotionaldiary.datasource.LocalDataSource
 import com.teamtuna.emotionaldiary.entity.DailyEmotion
 import com.teamtuna.emotionaldiary.entity.Emotion
+import com.teamtuna.emotionaldiary.entity.ErrorModel
+import com.teamtuna.emotionaldiary.entity.Result
 import com.teamtuna.emotionaldiary.map
 
 class EmotionRepositoryImpl(private val localDataSource: LocalDataSource) : EmotionRepository {
-    override suspend fun add(emotion: Emotion, reason: String): Long {
-        return localDataSource.add(emotion, reason)
+    override suspend fun add(
+        emotion: Emotion,
+        reason: String
+    ): Result<Long> {
+        return Result.Success(localDataSource.add(emotion, reason))
     }
 
-    override suspend fun get(id: Long): DailyEmotion {
-        return requireNotNull(localDataSource.get(id)).map()
+    override suspend fun get(id: Long): Result<DailyEmotion> {
+        return localDataSource.get(id)?.let {
+            Result.Success(it.map())
+        } ?: Result.Fail(ErrorModel())
+
     }
 }
