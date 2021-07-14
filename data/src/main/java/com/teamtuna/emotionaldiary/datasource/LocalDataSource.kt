4a866eb7e5ff3857474f65d95925713e38f72c7a
Robin <haveahopes@gmail.com>
2021-07-14 22:10:14 +0900

    #57 Modified Build Error

--- data/src/main/java/com/teamtuna/emotionaldiary/datasource/LocalDataSource.kt
+++ data/src/main/java/com/teamtuna/emotionaldiary/datasource/LocalDataSource.kt
@@ -5,7 +5,7 @@ import com.teamtuna.emotionaldiary.entity.Emotion
 import com.teamtuna.emotionaldiary.entity.UniqId
 
 interface LocalDataSource {
-    suspend fun add(emotion: Emotion, reason : String) : UniqId
+    suspend fun add(emotion: Emotion, reason: String): UniqId
 
-    suspend fun get(id : UniqId) : EmotionalEntity?
+    suspend fun get(id: UniqId): EmotionalEntity?
 }
