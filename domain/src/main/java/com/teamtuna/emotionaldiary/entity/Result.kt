4eb4f29e18ae6556b0fe816270a68d3d2a1e86e2
Pennya <rlawlgns077@gmail.com>
2021-07-14 20:43:45 +0900

    process inline

--- domain/src/main/java/com/teamtuna/emotionaldiary/entity/Result.kt
+++ domain/src/main/java/com/teamtuna/emotionaldiary/entity/Result.kt
@@ -5,4 +5,12 @@ sealed class Result<T> {
     class Fail<T>(val errorModel: ErrorModel?) : Result<T>()
 }
 
+inline fun <reified T> Result<T>.process(success: (T) -> Unit, error: (ErrorModel?) -> Unit) {
+    when (this) {
+        is Result.Success -> success(data)
+        is Result.Fail -> error(errorModel)
+    }
+}
+
+
 data class ErrorModel(val title: String? = "unKnown error", val description: String? = null)
--- presentation/src/main/java/com/teamtuna/emotionaldiary/add/EmotionAddViewModel.kt
+++ presentation/src/main/java/com/teamtuna/emotionaldiary/add/EmotionAddViewModel.kt
@@ -5,14 +5,16 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.teamtuna.emotionaldiary.entity.Emotion
+import com.teamtuna.emotionaldiary.entity.process
 import com.teamtuna.emotionaldiary.usecase.EmotionAddUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class EmotionAddViewModel @Inject constructor(
-        private val emotionAddUseCase: EmotionAddUseCase
+    private val emotionAddUseCase: EmotionAddUseCase
 ): ViewModel() {
 
     private val _response = MutableLiveData<Long>()
@@ -20,10 +22,12 @@ class EmotionAddViewModel @Inject constructor(
         get() = _response
 
     fun add(emotion: Emotion, reason: String) {
-        viewModelScope.launch {
-            emotionAddUseCase(emotion, reason).let {
+        viewModelScope.launch(Dispatchers.IO) {
+            emotionAddUseCase(emotion, reason).process({
                 _response.value = it
-            }
+            }, {
+
+            })
         }
     }
 
