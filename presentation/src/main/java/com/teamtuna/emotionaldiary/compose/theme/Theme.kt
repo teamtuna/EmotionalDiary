28cbc474d373a43f77856109a790b60505946d5e
eastar <pluto@eastandroid.com>
2021-08-23 16:58:31 +0900

    #82 hello compose sample add

---
+++ presentation/src/main/java/com/teamtuna/emotionaldiary/compose/theme/Theme.kt
@@ -0,0 +1,11 @@
+package com.teamtuna.emotionaldiary.compose.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun EmotionalDiaryTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        content = content
+    )
+}
