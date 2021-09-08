e362778d6f5b46cdb49d04c7e2bc2f8e7447b182
Robin <haveahopes@gmail.com>
2021-09-08 21:09:16 +0900

    87 : emotion-87: Toolbar추가

--- presentation/src/main/java/com/teamtuna/emotionaldiary/write/WriteCompose.kt
+++ presentation/src/main/java/com/teamtuna/emotionaldiary/write/WriteCompose.kt
@@ -1,33 +1,49 @@
 package com.teamtuna.emotionaldiary.write
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.google.accompanist.insets.ProvideWindowInsets
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.teamtuna.emotionaldiary.compose.theme.EmotionalDiaryTheme
 import com.teamtuna.emotionaldiary.entity.DailyEmotion
 import com.teamtuna.emotionaldiary.entity.Emotion
+import com.teamtuna.emotionaldiary.presentation.R
 import java.time.LocalDateTime
 
 @Preview
 @Composable
-fun WriteCompose() {
+fun WriteCompose(
+) {
     EmotionalDiaryTheme {
         ProvideWindowInsets {
             val systemUiController = rememberSystemUiController()
             SideEffect {
                 systemUiController.setSystemBarsColor(Color(0x55ff0000))
             }
-
             val emotion = DailyEmotion(
                 id = 0L,
                 imageUrl = "",
@@ -35,13 +51,50 @@ fun WriteCompose() {
                 emotion = Emotion.FEAR,
                 reason = "TEST"
             )
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight()
-            ) {
-                ImageCard(emotion)
-                WriteTextField(remember { mutableStateOf(TextFieldValue(text = emotion.reason)) })
+
+            val scrollState = rememberLazyListState()
+            Scaffold(
+                topBar = {
+                    InsetAwareTopAppBar(
+                        title = {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .wrapContentWidth(align = Alignment.CenterHorizontally)
+                                    .padding(start = 30.dp)
+                            ) {
+                                Text(
+                                    text = stringResource(id = R.string.app_name),
+                                    style = MaterialTheme.typography.subtitle2,
+                                    color = LocalContentColor.current,
+                                    modifier = Modifier
+                                        .padding(start = 10.dp)
+                                        .weight(1.5f)
+                                )
+                            }
+                        },
+                        navigationIcon = {
+                            IconButton(onClick = {}) {
+                                Icon(
+                                    imageVector = Icons.Filled.ArrowBack,
+                                    contentDescription = "",
+                                    tint = MaterialTheme.colors.primary
+                                )
+                            }
+                        },
+                        elevation = if (!scrollState.isScrollInProgress) 0.dp else 4.dp,
+                        backgroundColor = MaterialTheme.colors.surface
+                    )
+                }
+            ) { innerPadding ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                ) {
+                    ImageCard(emotion)
+                    WriteTextField(remember { mutableStateOf(TextFieldValue(text = emotion.reason)) })
+                }
             }
         }
     }
