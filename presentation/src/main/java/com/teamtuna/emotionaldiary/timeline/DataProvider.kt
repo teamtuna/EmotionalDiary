86614d43f9533bd084bd1d6bd61c304ab81ff111
karrel <karrel84@naver.com>
2021-09-08 23:51:41 +0900

    : emotion-88 - lint 처리

--- presentation/src/main/java/com/teamtuna/emotionaldiary/timeline/DataProvider.kt
+++ presentation/src/main/java/com/teamtuna/emotionaldiary/timeline/DataProvider.kt
@@ -9,52 +9,73 @@ object DataProvider {
         EmotionItem(
             emotion = Emotion.ANGER,
             diaryContent = "오늘은 화가 났다",
-            imageUrl = Uri.parse("https://upload2.inven.co.kr/upload/2016/07/19/bbs/i10597390041.jpg")
+            imageUrl = Uri.parse(
+                "https://upload2.inven.co.kr/" +
+                    "upload/2016/07/19/bbs/i10597390041.jpg"
+            )
         ),
         EmotionItem(
             emotion = Emotion.JOY,
             diaryContent = "오늘은 기뻣다",
-            imageUrl = Uri.parse("https://spnimage.edaily.co.kr/images/photo/files/NP/S/2019/05/PS19052700158.jpg")
+            imageUrl = Uri.parse(
+                "https://spnimage.edaily.co.kr/" +
+                    "images/photo/files/NP/S/2019/05/PS19052700158.jpg"
+            )
         ),
         EmotionItem(
             emotion = Emotion.SADNESS,
             diaryContent = "오늘은 슬프다",
             imageUrl = Uri.parse(
-                "https://cdn.pixabay.com/photo/2020/10/31/17/31/sad-5701778_1280.png"
+                "https://cdn.pixabay.com/" +
+                    "photo/2020/10/31/17/31/sad-5701778_1280.png"
             )
         ),
         EmotionItem(
             emotion = Emotion.ANGER,
             diaryContent = "오늘은 화가 났다",
-            imageUrl = Uri.parse("https://upload2.inven.co.kr/upload/2016/07/19/bbs/i10597390041.jpg")
+            imageUrl = Uri.parse(
+                "https://upload2.inven.co.kr/" +
+                    "upload/2016/07/19/bbs/i10597390041.jpg"
+            )
         ),
         EmotionItem(
             emotion = Emotion.JOY,
             diaryContent = "오늘은 기뻣다",
-            imageUrl = Uri.parse("https://spnimage.edaily.co.kr/images/photo/files/NP/S/2019/05/PS19052700158.jpg")
+            imageUrl = Uri.parse(
+                "https://spnimage.edaily.co.kr/" +
+                    "images/photo/files/NP/S/2019/05/PS19052700158.jpg"
+            )
         ),
         EmotionItem(
             emotion = Emotion.SADNESS,
             diaryContent = "오늘은 슬프다",
             imageUrl = Uri.parse(
-                "https://cdn.pixabay.com/photo/2020/10/31/17/31/sad-5701778_1280.png"
+                "https://cdn.pixabay.com/" +
+                    "photo/2020/10/31/17/31/sad-5701778_1280.png"
             )
         ),
         EmotionItem(
             emotion = Emotion.ANGER,
             diaryContent = "오늘은 화가 났다",
-            imageUrl = Uri.parse("https://upload2.inven.co.kr/upload/2016/07/19/bbs/i10597390041.jpg")
+            imageUrl = Uri.parse(
+                "https://upload2.inven.co.kr/" +
+                    "upload/2016/07/19/bbs/i10597390041.jpg"
+            )
         ),
         EmotionItem(
             emotion = Emotion.JOY,
             diaryContent = "오늘은 기뻣다",
-            imageUrl = Uri.parse("https://spnimage.edaily.co.kr/images/photo/files/NP/S/2019/05/PS19052700158.jpg")
+            imageUrl = Uri.parse(
+                "https://spnimage.edaily.co.kr/" +
+                    "images/photo/files/NP/S/2019/05/PS19052700158.jpg"
+            )
         ),
         EmotionItem(
             emotion = Emotion.SADNESS,
             diaryContent = "오늘은 슬프다",
             imageUrl = Uri.parse(
-                "https://cdn.pixabay.com/photo/2020/10/31/17/31/sad-5701778_1280.png"
+                "https://cdn.pixabay.com/" +
+                    "photo/2020/10/31/17/31/sad-5701778_1280.png"
             )
         ),
     )
