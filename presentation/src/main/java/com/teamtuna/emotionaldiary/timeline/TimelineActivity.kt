0fa6378fcfd7c8925578f5d9b4c498e9611551a5
karrel <karrel84@naver.com>
2021-09-09 00:03:09 +0900

    EMOTION-88 timeline compose 추가 (#102)
    * : - coil 적용
    - timeline recycler view 적용
    
    * : - test
    
    * : - test
    
    * : emotion-88
    - 사용하지 않는 import 제거
    
    * : emotion-88
    - kt lint
    
    * : emotion-88
    - kt lint
    
    * : emotion-88
    - kt lint
    
    * Update base.gradle
    
    Co-authored-by: eastar Jeong <438869+eastar-dev@users.noreply.github.com>
    
    * : emotion-88
    - 코드리뷰 수정
    
    * : emotion-88
    - lint 처리
    
    * : emotion-88
    - lint 처리
    
    * : emotion-88
    - lint 처리
    
    * : emotion-88
    - lint 처리
    
    Co-authored-by: eastar Jeong <438869+eastar-dev@users.noreply.github.com>

---
+++ presentation/src/main/java/com/teamtuna/emotionaldiary/timeline/TimelineActivity.kt
@@ -0,0 +1,15 @@
+package com.teamtuna.emotionaldiary.timeline
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teamtuna.emotionaldiary.presentation.R
+
+/**
+ * fixme TimelineFragment 개발을 위한 임시 액티비티
+ */
+class TimelineActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timeline)
+    }
+}
---
+++ presentation/src/main/java/com/teamtuna/emotionaldiary/timeline/TimelineFragment.kt
@@ -0,0 +1,124 @@
+package com.teamtuna.emotionaldiary.timeline
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.unit.dp
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
+import coil.transform.CircleCropTransformation
+
+class TimelineFragment : Fragment() {
+
+    private val viewModel: TimelineViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(
+                ViewCompositionStrategy.DisposeOnLifecycleDestroyed(viewLifecycleOwner)
+            )
+
+            setContent {
+                MaterialTheme {
+                    MyApp {
+                        Toast.makeText(requireContext(), "show detail", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun MyApp(navigateToDetail: (EmotionItem) -> Unit) {
+    Scaffold(
+        content = {
+            TimeLineContent(navigateToDetail)
+        }
+    )
+}
+
+@Composable
+fun TimeLineContent(navigateToDetail: (EmotionItem) -> Unit) {
+    val timeLine = remember { DataProvider.diaryList }
+
+    LazyColumn(contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)) {
+        items(
+            items = timeLine,
+            itemContent = {
+                TimeLineListItem(diary = it, navigateToDetail = navigateToDetail)
+            }
+        )
+    }
+}
+
+@Composable
+private fun TimeLineListItem(diary: EmotionItem, navigateToDetail: (EmotionItem) -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(horizontal = 8.dp, vertical = 8.dp)
+            .fillMaxWidth(),
+        elevation = 2.dp,
+        shape = RoundedCornerShape(corner = CornerSize(16.dp))
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(8.dp)
+        ) {
+            DiaryImage(diary)
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .align(Alignment.CenterVertically)
+                    .clickable { navigateToDetail(diary) }
+            ) {
+                Text(text = diary.emotion.toString(), style = MaterialTheme.typography.h5)
+                Text(text = diary.diaryContent)
+            }
+        }
+    }
+}
+
+@ExperimentalCoilApi
+@Composable
+private fun DiaryImage(diary: EmotionItem) {
+    Image(
+        painter = rememberImagePainter(data = diary.imageUrl, builder = {
+            transformations(CircleCropTransformation())
+        }),
+        contentDescription = null,
+        modifier = Modifier.size(64.dp)
+    )
+}
