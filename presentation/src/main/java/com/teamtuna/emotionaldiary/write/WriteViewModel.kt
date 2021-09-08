a66c4866c2cd1634dfee88704f703abae75b57cc
Robin <haveahopes@gmail.com>
2021-09-08 23:03:23 +0900

    87 : ktlint 수정

--- presentation/src/main/java/com/teamtuna/emotionaldiary/write/WriteViewModel.kt
+++ presentation/src/main/java/com/teamtuna/emotionaldiary/write/WriteViewModel.kt
@@ -5,5 +5,4 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class WriteViewModel @Inject constructor() : ViewModel() {
-}
+class WriteViewModel @Inject constructor() : ViewModel()
