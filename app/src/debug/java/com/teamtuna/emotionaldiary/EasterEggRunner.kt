be372257476078d61fca5942af3be78317b18a6a
eastar <eastarj@gmail.com>
2021-09-09 21:35:03 +0900

    emotion-108 : MaterialComponents bug fix #108

--- app/src/debug/java/com/teamtuna/emotionaldiary/EasterEggRunner.kt
+++ app/src/debug/java/com/teamtuna/emotionaldiary/EasterEggRunner.kt
@@ -2,6 +2,7 @@ package com.teamtuna.emotionaldiary
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.AlertDialog
 import android.app.Application
 import android.os.Bundle
 import android.view.View
@@ -9,7 +10,6 @@ import android.view.ViewGroup
 import android.view.Window
 import android.widget.SimpleAdapter
 import android.widget.TextView
-import androidx.appcompat.app.AlertDialog
 import androidx.core.content.pm.PackageInfoCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.doOnPreDraw
